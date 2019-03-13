22ceb02153878a10d7aa2279970cc31bcfffae16
Mark Christopher Cacal <markchristopher.cacal@outlook.com>
2019-03-13 11:58:58 +0800

    Hello Github

---
+++ Helpers/PrincipalHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using Microsoft.AspNet.Identity;
+using Models.Repository;
+
+namespace Helpers
+{
+    public static class PrincipalHelpers
+    {
+
+        public static bool IsInRoles(this IPrincipal principal, params string[] roles)
+        {
+            Models.Repository.UnitOfWork unitOfWork = new Models.Repository.UnitOfWork();
+            var userId = principal.Identity.GetUserId();
+            var user = unitOfWork.UsersRepo.Find(m => m.Id == userId);
+            if (principal == null)
+                return false;
+            if (user == null)
+                return false;
+            if (string.IsNullOrEmpty(userId))
+                return false;
+            return user.UserRoles.Any(m => roles.Contains(m.Name));
+        }
+
+        public static bool UserIsInRoles(string userId, params string[] roles)
+        {
+            var unitOfWork = new UnitOfWork();
+
+            var user = unitOfWork.UsersRepo.Find(m => m.Id == userId);
+
+            if (user == null)
+                return false;
+            if (string.IsNullOrEmpty(userId))
+                return false;
+            return user.UserRoles.Any(m => roles.Contains(m.Name));
+        }
+    }
+}
